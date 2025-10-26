29c1ba373e3719983c1025fdd4f336e01e922b4a
Jin <jinterlante@outlook.com>
2025-10-26 09:18:06 -0400

    Initial Commit

---
+++ AleutianControlPanelTests/AleutianControlPanelTests.swift
@@ -0,0 +1,17 @@
+//
+//  AleutianControlPanelTests.swift
+//  AleutianControlPanelTests
+//
+//  Created by Jin on 10/26/25.
+//
+
+import Testing
+@testable import AleutianControlPanel
+
+struct AleutianControlPanelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
